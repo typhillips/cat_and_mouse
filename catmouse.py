acdd70f83ffb11f03ba81f219fefd2a5188d4efc
Ty Phillips <typhillips73@gmail.com>
2015-12-01 21:51:40 -0600

    Added cat positioning based on up, down, left, right keys

--- catmouse.py
+++ catmouse.py
@@ -2,13 +2,17 @@ import sys
 import random
 import pygame
 
-pygame.init()
-
 # Constants
-screenSize = (600, 400)
-backgroundPict = "grass.png"	# 600x400 pixels
-catPict = "cat.png"				# 80x58 pixels
-mousePict = "mouse.png"			# 50x49 pixels
+SCREEN_SIZE = (600, 400)
+BACKGROUND_PICT = "grass.png"		# For now, background pict must be same dimenstions as screenSize (600x400 pixels)
+CAT_PICT = "cat.png"				# 80x58 pixels
+MOUSE_PICT = "mouse.png"			# 50x49 pixels
+catSpeed = 7						# Pixels to move for each keypress
+waitTime = 10						# Time delay in ms for each loop
+
+class CatMouseGame(object):
+	def __init__(self):
+		pass
 
 class Cat(pygame.sprite.Sprite):
 	"""Cat class"""
@@ -16,7 +20,7 @@ class Cat(pygame.sprite.Sprite):
 		"""Creates a cat given an (x,y) tuple with specified starting position."""
 		pygame.sprite.Sprite.__init__(self)		# Call the parent class (Sprite) constructor
 
-		self.image = pygame.image.load(catPict).convert_alpha()
+		self.image = pygame.image.load(CAT_PICT).convert_alpha()
 
 		# Fetch the rectangle object that has the dimensions of the image
 		# Update the position of this object by setting the values # of rect.x and rect.y
@@ -32,7 +36,7 @@ class Mouse(pygame.sprite.Sprite):
 		"""Creates a mouse given an (x,y) tuple with specified starting position."""
 		pygame.sprite.Sprite.__init__(self)		# Call the parent class (Sprite) constructor
 
-		self.image = pygame.image.load(mousePict).convert_alpha()
+		self.image = pygame.image.load(MOUSE_PICT).convert_alpha()
 
 		# Fetch the rectangle object that has the dimensions of the image
 		# Update the position of this object by setting the values # of rect.x and rect.y
@@ -43,45 +47,54 @@ class Mouse(pygame.sprite.Sprite):
 		self.rect.y = newPos[1]
 
 
-screen = pygame.display.set_mode(screenSize)
-background = pygame.image.load("grass.png").convert_alpha()
+if __name__ == "__main__":
+	pygame.init()
+	screen = pygame.display.set_mode(SCREEN_SIZE)
+	background = pygame.image.load(BACKGROUND_PICT).convert_alpha()
 
-#mouse = pygame.image.load("mouse.png").convert_alpha()
-#cat = pygame.image.load("cat.png").convert_alpha()
-
-# Place the cat in a random location at the start of the game
-#catx = random.randrange
-cat = Cat()
-catx = random.randrange(1, screenSize[0] - cat.rect.size[0])
-caty = random.randrange(1, screenSize[1] - cat.rect.size[1])
-cat.update((catx, caty))
-
-mouse = Mouse()
-mousex = random.randrange(1, screenSize[0] - mouse.rect.size[0])
-mousey = random.randrange(1, screenSize[1] - mouse.rect.size[1])
-mouse.update((mousex, mousey))
-
-# Create group (allgroup) for sprites to belong to
-allgroup = pygame.sprite.Group()
-allgroup.add(cat)
-allgroup.add(mouse)
-
-# Main loop
-while True:
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT:
-			sys.exit()
-
-	screen.blit(background, (0, 0))	# Display background image (same size as screen)
-	allgroup.clear(screen, background)
-	#allgroup.update((1,1))
-	allgroup.draw(screen)
-	pygame.display.flip()          # flip the screen 30 times a second
-
-	catx += 1
-	caty += 1
+	# Place both cat and mouse at random start location
+	cat = Cat()
+	catx = random.randrange(1, SCREEN_SIZE[0] - cat.rect.size[0])
+	caty = random.randrange(1, SCREEN_SIZE[1] - cat.rect.size[1])
 	cat.update((catx, caty))
-	mousex += 2
-	mousey += 2
+
+	mouse = Mouse()
+	mousex = random.randrange(1, SCREEN_SIZE[0] - mouse.rect.size[0])
+	mousey = random.randrange(1, SCREEN_SIZE[1] - mouse.rect.size[1])
 	mouse.update((mousex, mousey))
-	pygame.time.wait(10)
+
+	# Create group (allgroup) for sprites to belong to
+	allgroup = pygame.sprite.Group()
+	allgroup.add(cat)
+	allgroup.add(mouse)
+
+	# Main loop
+	while True:
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				sys.exit()
+
+		screen.blit(background, (0, 0))	# Display background image (same size as screen)
+		allgroup.clear(screen, background)
+		allgroup.draw(screen)
+		pygame.display.flip()
+
+		# Move the cat based on key presses
+		keyState = pygame.key.get_pressed()
+
+		if keyState[pygame.K_UP]:
+			caty -= catSpeed
+		if keyState[pygame.K_DOWN]:
+			caty += catSpeed
+		if keyState[pygame.K_LEFT]:
+			catx -= catSpeed
+		if keyState[pygame.K_RIGHT]:
+			catx += catSpeed
+
+		cat.update((catx, caty))	# Update cat sprite position
+
+		# Test code to move cat and mouse diagonally down & to the right @different speeds
+		mousex += 2
+		mousey += 2
+		mouse.update((mousex, mousey))
+		pygame.time.wait(waitTime)
